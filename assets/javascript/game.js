6f415a00efb4849559cb2600538f39142d2ad2e4
Scott H <zenwattage@gmail.com>
2019-03-07 18:43:14 -0800

    insitial styling commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+
+
+//theme: 80's movies
+
+//array of movies
+var movies = [
+    "Gremlins",
+    "Beetlejuice",
+    "Wargames",
+    "Caddyshack",
+    "Labryinth"
+];
+//choose movie from array
+
+//move title = length
+console.log(...movies);
+//function for checking user guesses against an array
+
