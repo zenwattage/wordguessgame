5785ec60605750d0f684f2b1ead0d787c956a145
Scott H <zenwattage@gmail.com>
2019-03-10 12:57:11 -0700

    logic update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,24 +22,77 @@ var movies = [
     //prevent repeats
 
     var random = movies[Math.floor(Math.random() * movies.length)];
-    console.log(random);
+    
 
+    var chosenWord = random;
+    //console.log(typeof(chosenWord));
 
     //fill empty array with spaces equal to letters in chosen word
     var guessList = [];
-    for(var i = 0; i < random.length; i++) {
+    for(var i = 0; i < chosenWord.length; i++) {
         guessList[i] = "_";
     }
-    console.log(guessList);
 
+    //fill empty array with chosenWord seperated into characters
+    var letterList = [];
+    for(var g = 0; g < chosenWord.length; g++) {
+        letterList[g] = chosenWord[g];
+        console.log(letterList);
+    }
+
+    //console.log(guessList);
+    
+    //DISPLAY CURRENT SCORE
     
     //display dashes on page
     document.getElementById("spaces").innerHTML = guessList.join("");
-    document.getElementById("letters").innerHTML = random;
 
-    var blankSpaces = random.length;
+    //display chosen word on page
+    document.getElementById("letters").innerHTML = chosenWord;
 
-    console.log(blankSpaces);
+    //amount of turns is the amount of letters in the chosen word
+    //lose turns on incorrect guesses
+    
+    var turns= chosenWord.length;
+    
+    console.log("Turns Remaining: " + turns);
+
+    //console.log(typeof(random));
+
+    //while turns > 0 {
+    
+    //get key pressed by user
+    var keyPressed = "";
+    addEventListener("keyup", function(event) {
+        keyPressed = String(event);
+        console.log("keyPressed type is: " + typeof(keyPressed));
+        //convert keyPressed from object to char
+
+
+
+        for(var lettercheck = 0; lettercheck < letterList.length; lettercheck++) {
+            console.log(keyPressed);
+            if(keyPressed == letterList[lettercheck]){
+                console.log(keyPressed);
+                console.log(letterList[lettercheck]);
+            }
+        }
+
+
+        
+    })
+        
+    //compare key pressed to chosen word (random)
+
+    //if keypressed is in the chosen word
+    //update spaces with correct letter(keypress)
+    //if keypressed is not in chosen word
+    //
+    
+
+
+
+
 
 
 
