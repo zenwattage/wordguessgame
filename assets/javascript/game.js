7081cb6ff09d230692f274130496c4735a3514ec
Scott H <zenwattage@gmail.com>
2019-03-13 13:16:25 -0700

    fixed output issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,18 +19,16 @@ var movies = [
 
 
     //game chooses title from array randomly
-    //prevent repeats
-
     var movie = movies[Math.floor(Math.random() * movies.length)];
-    
+    //NOT PREVENTING REPEATS    
 
-    var chosenWord = movie;
+    var chosenWord = movie.toLowerCase();
     //console.log(typeof(chosenWord));
 
     //fill empty array with spaces equal to letters in chosen word
-    var guessList = [];
+    var spaceList = [];
     for(var i = 0; i < chosenWord.length; i++) {
-        guessList[i] = "_";
+        spaceList[i] = "_";
     }
 
     //fill empty array with chosenWord seperated into characters
@@ -40,20 +38,33 @@ var movies = [
         console.log(letterList);
     }
 
-    //console.log(guessList);
+    var guessList = [];
+    //fill array with non mathcing pressed key
+   /*  var guessList = [];
+    addEventListener("keydown", function(event){
+        for(var b = 0; b < chosenWord.length; b++) {
+            if(chosenWord[b] !== keyPressed) {
+                guessList[b] = keyPressed;
+            }
+        }
+
+
+    }) */
     
-    //DISPLAY CURRENT COS
-    
+        
+
+      
     //display dashes on page
-    document.getElementById("spaces").innerHTML = guessList.join("");
+    document.getElementById("spaces").innerHTML = spaceList.join("");
 
     //display chosen word on page
-    document.getElementById("letters").innerHTML = chosenWord;
+    //document.getElementById("letters").innerHTML = letterList.join("");
 
     //amount of turns is the amount of letters in the chosen word
     //lose turns on incorrect guesses
     
-   var turns= parseInt(chosenWord.length); 
+   var turns= parseInt(chosenWord.length);
+
     
     console.log("Turns Remaining: " + turns);
 
@@ -62,35 +73,35 @@ var movies = [
     //get key pressed by user & convert it to a
     var keyPressed = "";
     addEventListener("keyup", function(event) {
-        keyPressed = event.key; 
-        console.log(typeof(keyPressed));
+        keyPressed = event.key;
+        console.log(keyPressed);
 
         for(var j = 0; j < chosenWord.length; j++) {
-            
             //if the index of the chosen word matches key
             if(chosenWord[j] === keyPressed){
                 //update dashed line with character pressed
                 rightAnSound.play();
-                guessList[j] = String(keyPressed);
+                
+                spaceList[j] = String(keyPressed);
                 //console.log("In the Word: "+chosenWord[j]);
+            } else {
+                guessList[j] = keyPressed;
+                console.log(guessList);
             }
-            
-            else{(chosenWord[j] != keyPressed){
-                turns--;
-            }
+           
         }
-            
-            /* else {
+                /* else {
                 //play wrong answer sound
                 wrongAnSound.play();
                 turns--
                 
             } */
-        }
+        
 
         //write score to screen
+        document.getElementById("wrongGuesses").innerHTML = "Guessed Letters: " + guessList;
         document.getElementById("score").innerHTML = "Turns Remaining: " + turns;
-        document.getElementById("spaces").innerHTML = guessList.join(chosenWord[j]);
+        document.getElementById("spaces").innerHTML = spaceList.join("",chosenWord[j]);
         //document.getElementById("imageTest").innerHTML = 
 
 
@@ -104,25 +115,3 @@ var movies = [
        var rightAnSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/goodbell.mp3");
        var winSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/win.mp3");
        var loseSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/lose.mp3");
-            
-      
-        
-    //compare key pressed to chosen word (random)
-
-    //if keypressed is in the chosen word
-    //update spaces with correct letter(keypress)
-    //if keypressed is not in chosen word
-    //
-    
-
-
-
-
-
-/* 
-
-//sounds
-    this.goodSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/goodbell.mp3"),
-    this.badSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/bad.mp3"),
-    this.winSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/win.mp3"),
-    this.loseSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/lose.mp3") */
