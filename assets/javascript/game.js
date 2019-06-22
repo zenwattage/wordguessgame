466befc5b7ee7a1d32ccc5b7c24fd974bc3de6e9
Scott H <zenwattage@gmail.com>
2019-06-21 23:34:32 -0700

    rearranging and cleaning up variable names

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 //REFACTOR
 //theme: 80's movies
 
-var wrongGuesses = 10; 
+var wrongGuesses = 10;
 var spaceList = [];
 var letterList = [];
 var guessList = [];
@@ -23,72 +23,72 @@ var movies = [
     "Legend"
 ];
 
-    //GAME VIEW UPDATE FUNCTION
-    //GET STATE OF GAME FUNCTION
-    //CHECK WINSTATE FUNCTION
-    //RESET GAME FUNCTION
+//GAME VIEW UPDATE FUNCTION
+//GET STATE OF GAME FUNCTION
+//CHECK WINSTATE FUNCTION
+//RESET GAME FUNCTION
 
-    //check if keys are between a(65)-z(90)
-    //if(event.keyPressed >= 65 && event.keyPressed <= 90)
+//check if keys are between a(65)-z(90)
+//if(event.keyPressed >= 65 && event.keyPressed <= 90)
 
-    //game chooses title from array randomly
-    var movie = movies[Math.floor(Math.random() * movies.length)];
-    //NOT PREVENTING REPEATS    
+//game chooses title from array randomly
+var movie = movies[Math.floor(Math.random() * movies.length)];
+//NOT PREVENTING REPEATS    
 
-    var chosenWord = movie.toLowerCase();
+var chosenWord = movie.toLowerCase();
 
-    //fill empty array with spaces equal to letters in chosen word
-    
-    for(var i = 0; i < chosenWord.length; i++) {
-        spaceList[i] = "_";
-    }
+//fill empty array with spaces equal to letters in chosen word
 
-    //fill empty array with chosenWord seperated into characters
-    
-    for(var g = 0; g < chosenWord.length; g++) {
-        letterList[g] = chosenWord[g];
-        console.log(letterList);
-    }
+for (var i = 0; i < chosenWord.length; i++) {
+    spaceList[i] = "_";
+}
 
-    
-      
-    //display dashes on page
-    document.getElementById("spaces").innerHTML = spaceList.join("");
+//fill empty array with chosenWord seperated into characters
 
-    var keyPressed = "";
+for (var g = 0; g < chosenWord.length; g++) {
+    letterList[g] = chosenWord[g];
+    console.log(letterList);
+}
 
-    addEventListener("keyup", function(event) {
-        keyPressed = event.key;
-        //console.log(keyPressed);
 
-        for(var j = 0; j < chosenWord.length; j++) {
-            //if the index of the chosen word matches key
-            if(chosenWord[j] === keyPressed){
-                //update dashed line with character pressed
-                rightAnSound.play();
-                
-                spaceList[j] = String(keyPressed);
-                //console.log("In the Word: "+chosenWord[j]);
-            } else {
-                guessList.push(keyPressed);
-            }
-           
+
+//display dashes on page
+document.getElementById("spaces").innerHTML = spaceList.join("");
+
+var keyPressed = "";
+
+addEventListener("keyup", function (event) {
+    keyPressed = event.key;
+    //console.log(keyPressed);
+
+    for (var j = 0; j < chosenWord.length; j++) {
+        //if the index of the chosen word matches key
+        if (chosenWord[j] === keyPressed) {
+            //update dashed line with character pressed
+            rightAnSound.play();
+
+            spaceList[j] = String(keyPressed);
+            //console.log("In the Word: "+chosenWord[j]);
+        } else {
+            guessList.push(keyPressed);
         }
 
-        
-        //write score to screen
-        document.getElementById("wrongGuesses").innerHTML = "Guessed Letters: " + guessList;
-        document.getElementById("score").innerHTML = "Turns Remaining: " + turns;
-        document.getElementById("spaces").innerHTML = spaceList.join("");
-        
-
-        })
+    }
 
 
+    //write score to screen
+    document.getElementById("wrongGuesses").innerHTML = "Guessed Letters: " + guessList;
+    document.getElementById("score").innerHTML = "Turns Remaining: " + turns;
+    document.getElementById("spaces").innerHTML = spaceList.join("");
 
 
-        //sounds
-       var wrongAnSound = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/bad.mp3');
-       var rightAnSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/goodbell.mp3");
-       var winSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/win.mp3");
-       var loseSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/lose.mp3");
+})
+
+
+
+
+//sounds
+var wrongAnSound = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/bad.mp3');
+var rightAnSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/goodbell.mp3");
+var winSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/win.mp3");
+var loseSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/lose.mp3");
