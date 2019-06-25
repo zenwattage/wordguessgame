91f27cab56d6b6d66925d7994c4cf41fb86961d9
Scott H <zenwattage@gmail.com>
2019-06-25 10:36:50 -0700

    repushing after internet outtage

---
+++ maptesting.js
@@ -0,0 +1,14 @@
+var movies = [
+    {
+        name: "Rocky",
+        rating: "PG",
+        year: "1990"
+    },
+    {
+        name: "Rocky II",
+        rating: "PG-13",
+        year: "1994"
+    }
+]
+
+console.log(movies.name);
